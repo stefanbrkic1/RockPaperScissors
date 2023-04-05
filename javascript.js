5161a0cfa1b27f9e09daa2276d97c95e6d0dcc23
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-04-05 17:23:23 +0200

    Finished Project
    Project Finished.

--- javascript.js
+++ javascript.js
@@ -1,66 +1,74 @@
-function getComputerChoice(){
-    const x="paper";
-    const y="rock";
-    const z="scissors";
+document.addEventListener("DOMContentLoaded", function () {
 
-    let randomChoice = Math.floor(Math.random()*3);
-
-    if (randomChoice===0){
-        let computerChoice=x;
-        return computerChoice;
-    }
-    else if(randomChoice===1){
-        let computerChoice=y;
-        return computerChoice;
-    }
-    else{
-        let computerChoice=z;
-        return computerChoice;
-    }
-}
-
-
-function playRound(playerSelection, computerSelection){
-
-    let result;
-
-    if(playerSelection==='rock' && computerSelection==='rock' || playerSelection==='paper' && computerSelection==='paper' || playerSelection==='scissors' && computerSelection==='scissors'){
-        result="It's a tie";
-        return result;
-    }
-    else if(playerSelection==='rock' && computerSelection==='paper' || playerSelection==='paper' && computerSelection==='scissors' || playerSelection==='scissors' && computerSelection==='rock'){
-        result=`You've lost. ${computerSelection} beats ${playerSelection}`;
-        return result;
-    }
-    else{
-        result=`You've won. ${playerSelection} beats ${computerSelection}`;
-        return result;
-    }
-
-}
-
-
-let userScore =0 ;
-let computerScore = 0;
-
-function game(){
+    let userChoice;
+    let computerChoice;
+  
+  
+    let playerScore = 0;
+    let computerScore = 0;
+  
     
-    for (let i = 0; i < 5; i++) 
-    {
-        const playerSelection=prompt('Enter your selection(rock/paper/scissors)').toLowerCase();;
-        const computerSelection=getComputerChoice();
-        const gameResult=playRound(playerSelection, computerSelection);
-
-        console.log(gameResult);
-
-        if(gameResult===`You've lost. ${computerSelection} beats ${playerSelection}`){
-            computerScore ++;
-        }
-        else if(gameResult===`You've won. ${playerSelection} beats ${computerSelection}`){
-            userScore ++;
-        }
+    const btnBox1 = document.getElementById("btn-box1");
+    const btnBox2 = document.getElementById("btn-box2");
+    const btnBox3 = document.getElementById("btn-box3");
+    const scoreNumber1 = document.querySelector(".score-number:first-of-type");
+    const scoreNumber2 = document.querySelector(".score-number:last-of-type");
+    const resultText = document.querySelector(".result-text");
+  
+    
+    function getComputerChoice() {
+      const choices = ["rock", "paper", "scissors"];
+      const randomNumber = Math.floor(Math.random() * 3);
+      return choices[randomNumber];
     }
-}
-   game();
-
-console.log(`Final score: USER: ${userScore} - COMPUTER ${computerScore}`);
+  
+    
+    function updateScore(result) {
+      if (result === "win") {
+        playerScore++;
+        scoreNumber1.textContent = playerScore;
+        resultText.textContent = "You WON";
+      } else if (result === "lose") {
+        computerScore++;
+        scoreNumber2.textContent = computerScore;
+        resultText.textContent = "You LOST";
+      } else {
+        resultText.textContent = "It's a TIE";
+      }
+    }
+  
+  
+    function checkWin(userChoice, computerChoice) {
+      if (userChoice === computerChoice) {
+        updateScore("tie");
+      } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+      ) {
+        updateScore("win");
+      } else {
+        updateScore("lose");
+      }
+    }
+  
+    
+    btnBox1.addEventListener("click", function () {
+      userChoice = "rock";
+      computerChoice = getComputerChoice();
+      checkWin(userChoice, computerChoice);
+    });
+  
+    btnBox2.addEventListener("click", function () {
+      userChoice = "paper";
+      computerChoice = getComputerChoice();
+      checkWin(userChoice, computerChoice);
+    });
+  
+    btnBox3.addEventListener("click", function () {
+      userChoice = "scissors";
+      computerChoice = getComputerChoice();
+      checkWin(userChoice, computerChoice);
+    });
+  });
+  
